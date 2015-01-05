cec7ea3cbcaa4b73b96a75cce6fbf1a89e0dc6c3
Mark Anderson <mark@revelry.co>
2015-01-05 14:07:05 -0600

    test_connection displays status on load, not button click required

--- app/controllers/weblinc/admin/avatax_test_connections_controllers.rb
+++ app/controllers/weblinc/admin/avatax_test_connections_controllers.rb
@@ -1,31 +1,35 @@
 module Weblinc
   class Admin::AvataxTestConnectionsController < Admin::ApplicationController
     def show
-       current_settings = Weblinc::Avatax::Setting.current
-       current_settings.apply_settings
-       @settings = current_settings.settings_edit_hash
+      current_settings = Weblinc::Avatax::Setting.current
+      current_settings.apply_settings
+      ping
+      @settings = current_settings.settings_edit_hash
     end
 
     def update
-
-      taxSvc = AvaTax::TaxService.new
-
-      begin  # catch exception if service URL is not valid
-        pingResult = taxSvc.ping
-        if pingResult["ResultCode"] == "Success"
-          flash[:success] = 'AvaTax connecton successful.'
-        else
-          flash[:error] = pingResult["Messages"].collect { |message| message["Summary"] }
-        end
-      rescue Exception => e  
-          flash[:error] = e.message
-      end
-
-
       redirect_to avatax_test_connection_path
     end
 
     private
+    def ping
+      taxSvc = AvaTax::TaxService.new
+      begin  # catch exception if service URL is not valid
+        pingResult = taxSvc.ping
+        if pingResult["ResultCode"] == "Success"
+          @connection = {:status => 'Service Available', :errors => []}
+        else
+          @connection = {:status => 'Errors'}
+          @connection[:errors] = pingResult["Messages"].collect { |message| message["Summary"] }
+          flash[:error] = 'Failure'
+        end
+      rescue Exception => e  
+        throw :mra
+        @connection = {:status => "Exception"}
+        @connection[:errors] = [e.message]
+        flash[:error] = 'Exception'
+      end
+    end
  
   end
 end
