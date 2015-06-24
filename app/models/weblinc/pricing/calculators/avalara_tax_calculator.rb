f814c77dbffd1f851a40f5645c98e62d23d468b4
Adam Clarke <adamclarkewastaken@gmail.com>
2015-06-24 11:53:26 -0500

    tax service refactors

--- app/models/weblinc/pricing/calculators/avalara_tax_calculator.rb
+++ app/models/weblinc/pricing/calculators/avalara_tax_calculator.rb
@@ -1,44 +1,45 @@
 module Weblinc
   module Pricing
     module Calculators
+      # calculates order/shipment sales tax via the Avalara Avatax API
       class AvalaraTaxCalculator
         include Calculator
-
-        def taxable_items
-          @taxable_items ||= order.items.select do |item|
-            item.price_adjustments.sum > 0
-          end
-        end
-
-        def shipping_total
-          @shipping_total ||= order.shipping_method.price_adjustments.sum
-        end
 
         def adjust
           return if shipments.map(&:address).compact.empty?
           return unless order.call_avatax_api_flag
           response = Weblinc::Avatax::TaxService.new(order, shipments).get
 
-          order.items.each do |item|
-            response.order_item_lines(item.id.to_s).each do |tax_line|
-              item.adjust_pricing(
-                price: 'tax',
-                calculator: self.class.name,
-                description: 'Sales Tax',
-                amount: tax_line['Tax'].to_m
-              )
-            end
-          end
+          adjust_items(response)
+          adjust_shipments(response)
+        end
 
+        private
+
+        def adjust_items(response)
+          order.items.each do |item|
+            lines = response.order_item_lines(item.id.to_s)
+            adjust_pricing(item, lines)
+          end
+        end
+
+        def adjust_shipments(response)
           shipments.each do |shipment|
-            response.shipment_lines(shipment) do |tax_line|
-              shipment.adjust_pricing(
-                price: 'tax',
-                calculator: self.class.name,
-                description: 'Sales Tax',
-                amount: tax_line['Tax'].to_m
-              )
-            end
+            lines = response.shipment_lines(shipment.id.to_s)
+            adjust_pricing(shipment, lines)
+          end
+        end
+
+        # takes a shipment or order item and applies the tax lines found in
+        # response from the Avatax API
+        def adjust_pricing(taxable, lines)
+          lines.each do |tax_line|
+            taxable.adjust_pricing(
+              price: 'tax',
+              calculator: self.class.name,
+              description: 'Sales Tax',
+              amount: tax_line['Tax'].to_m
+            )
           end
         end
       end
