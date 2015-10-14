d4d1a4bce6cb3826bd303088a84ae8508ff46f7e
Jeff Yucis <jyucis@weblinc.com>
2015-10-14 14:17:59 -0400

    Update initializer for core compatibility.

--- config/initializers/avatax.rb
+++ config/initializers/avatax.rb
@@ -1,6 +1,6 @@
 
 # swap default tax calculator for AvaTax
-Weblinc::Pricing.shipment_calculators.swap(
+Weblinc::Pricing.calculators.swap(
   Weblinc::Pricing::Calculators::TaxCalculator,
   Weblinc::Pricing::Calculators::AvalaraTaxCalculator
 )
