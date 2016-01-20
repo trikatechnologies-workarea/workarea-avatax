a9d852b706c781c10cdab6f55d0347922e5d1b37
Bryan Alexander <balexander@weblinc.com>
2016-01-20 12:03:55 -0500

    RACHELROY-67: Add some spacing, remove more "wl-" in css classes

--- app/decorators/weblinc/store_front/checkouts_controller_decorator.rb
+++ app/decorators/weblinc/store_front/checkouts_controller_decorator.rb
@@ -2,15 +2,15 @@ module Weblinc
   decorate(StoreFront::CheckoutsController, with: 'avatax') do # Specify the plugin name when decorating
     class_methods do
       def setup_view_models
-        current_order.call_avatax_api_flag= true
+        current_order.call_avatax_api_flag = true
         super
-        current_order.call_avatax_api_flag= false
+        current_order.call_avatax_api_flag = false
       end
 
       def place_order
-        current_order.call_avatax_api_flag= true
+        current_order.call_avatax_api_flag = true
         super
-        current_order.call_avatax_api_flag= false
+        current_order.call_avatax_api_flag = false
       end
     end
   end
