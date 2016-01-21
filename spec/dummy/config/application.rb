d74a8a8b11163e115571d25f322daa72a82eaa69
Bryan Alexander <balexander@weblinc.com>
2016-01-21 10:16:28 -0500

    RACHELROY-67: Move string into config variable

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -10,17 +10,9 @@ require "avatax"
 
 module Dummy
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    Weblinc.configure do |config|
+      config.avatax_default_service_url = 'https://development.avalara.net'
+    end
   end
 end
 
