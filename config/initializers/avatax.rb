e2c1c1c4062b8a055f93f38b6863d9535b80377e
Jeff Yucis <jyucis@weblinc.com>
2016-11-03 12:25:40 -0400

    Revert initializer

--- config/initializers/avatax.rb
+++ config/initializers/avatax.rb
@@ -1,13 +1,5 @@
-if Rails.env.test?
-  Weblinc::Pricing.calculators.swap(
-    Weblinc::Pricing::Calculators::AvalaraTaxCalculator,
-    Weblinc::Pricing::Calculators::TaxCalculator
-  )
-  # This will effectively disable the listener that publishes
-  # transactions to Avatax.
-  Weblinc::Avatax::Setting.current.update_attribute(:doc_handling, :none)
-else
-  Weblinc::Avatax::Setting.current.update_attributes!(
-    Weblinc.config.avatax
-  )
-end
+# swap default tax calculator for AvaTax
+Weblinc::Pricing.calculators.swap(
+  Weblinc::Pricing::Calculators::TaxCalculator,
+  Weblinc::Pricing::Calculators::AvalaraTaxCalculator
+)
